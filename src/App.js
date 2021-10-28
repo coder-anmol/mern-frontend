fa58a2503d9d525c18d4451912d6dfc02721c16e
Anmol Sharma <dev.anmolsharma@gmail.com>
2021-10-29 05:04:59 +0530

    pages created and routing added on them

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div>
-      <p>Our Frontend Is Ready To Build</p>
-    </div>
+    <Router>
+      <div>
+        {/* navbar */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/blog">Blog</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and renders the first one that matches the current URL. */}
+
+        {/* Routing added */}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/blog" component={Blog} />
+        </Switch>
+
+        {/* Footer */}
+        <footer>MERN-Frontend</footer>
+      </div>
+    </Router>
   );
 }
 
